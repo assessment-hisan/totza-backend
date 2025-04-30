3285ea127a8532e41f079906baf58e6c51458ddb
assessment-hisan <assessment.hisan@gmail.com>
2025-04-30 12:12:28 +0530

    Update companyTransactionRoutes.js

--- src/routes/companyTransactionRoutes.js
+++ src/routes/companyTransactionRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createCompanyTransaction, getCompanyTransactions, deleteCompanyTransaction } from '../controllers/companyTransactionController.js';
+import { createCompanyTransaction, getCompanyTransactions, deleteCompanyTransaction, getRecentCompanyTransactions } from '../controllers/companyTransactionController.js';
 import {authMiddleware} from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/', authMiddleware, createCompanyTransaction);
 router.get('/', authMiddleware, getCompanyTransactions);
+router.get('/recent', authMiddleware, getRecentCompanyTransactions);
 router.delete('/:id', authMiddleware, deleteCompanyTransaction);
 
 export default router;
